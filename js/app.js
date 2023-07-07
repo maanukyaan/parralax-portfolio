55f731d3dac2d9f4afe4c5e6f4940a2e8eb9876f
Vahe <8384838z@gmail.com>
2023-07-07 04:09:24 +0300

    Added custom cursor, UFO changed to saturn

--- js/app.js
+++ js/app.js
@@ -6,3 +6,30 @@ document.addEventListener("mousemove", (e) => {
         `,
   });
 });
+
+const innerCursor = document.querySelector(".inner-cursor");
+const outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  innerCursor.style.left = `${x}px`;
+  innerCursor.style.top = `${y}px`;
+  outerCursor.style.left = `${x}px`;
+  outerCursor.style.top = `${y}px`;
+}
+
+const elems = document.querySelectorAll(".custom-hover");
+
+elems.forEach((elem) => {
+  elem.addEventListener("mouseover", () => {
+    innerCursor.classList.add("grow");
+  });
+
+  elem.addEventListener("mouseleave", () => {
+    innerCursor.classList.remove("grow");
+  });
+});
